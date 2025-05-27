34194be3e2b52c386a9f241a678400350050ae71
Andrey Sorokin <asassinovin@gmail.com>
2025-05-27 19:38:06 +0300

    создал файл под каталог, разделил проект на соствляющие(html+css+js). настроил окружение

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Пока что Alpine.js не требует JS здесь
+// Если появится калькулятор или раскрывающиеся блоки — добавим
+document.addEventListener('alpine:init', () => {
+  console.log('Alpine.js подключен');
+});
